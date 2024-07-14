6f48c2b148a32c3c08ddff67e0273baac2930295
yoichiro-yamamoto <yoichiro.yamamoto@dena.com>
2024-07-15 00:37:28 +0900

    動画コメント

--- src/app/video/VideoReviewTool.tsx
+++ src/app/video/VideoReviewTool.tsx
@@ -1,24 +1,61 @@
 'use client';
 
 import React, { useState, useRef, useEffect } from 'react';
-import { Stage, Layer, Text, Circle } from 'react-konva';
-import type ReactPlayerType from 'react-player';
-import dynamic from 'next/dynamic'
+import { Stage, Layer, Text, Circle, Line, Group } from 'react-konva';
+import ReactPlayer from 'react-player';
+import { Modal, Input } from 'antd';
+import Konva from 'konva';
+import dynamic from 'next/dynamic';
+const DynamicReactPlayer = dynamic(
+  () => import('react-player'),
+  { ssr: false }, // SSRを無効化
+);
 
-interface Comment {
-  x: number;
-  y: number;
-  text: string;
-  time: number;
-}
+const ReactPlayerMemo = React.memo(
+  ({
+    handleProgress,
+  }: {
+    handleProgress: (state: {
+      playedSeconds: number;
+      played: number;
+      loadedSeconds: number;
+      loaded: number;
+    }) => void;
+  }) => {
+    const playerRef = useRef(null);
+
+    return (
+      <DynamicReactPlayer
+        ref={playerRef}
+        url="assets/Big_Buck_Bunny_360_10s_1MB.mp4"
+        width="100%"
+        height="100%"
+        onProgress={handleProgress}
+        style={{ position: 'absolute', top: 0, left: 0 }}
+        controls={true}
+        onError={(e) => console.error('ReactPlayer error:', e)}
+        onReady={() => console.log('ReactPlayer is ready')}
+        config={{ file: { attributes: { crossOrigin: 'anonymous' } } }}
+      />
+    );
+  },
+);
 
 const VideoReviewTool: React.FC = () => {
+  type Comment = {
+    x: number;
+    y: number;
+    text: string;
+    time: number;
+    videoWidth: number;
+    videoHeight: number;
+  };
+
   const [comments, setComments] = useState<Comment[]>([]);
-  const [currentTime, setCurrentTime] = useState(5);
+  const [currentTime, setCurrentTime] = useState(0);
   const [dimensions, setDimensions] = useState({ width: 640, height: 360 });
-  const playerRef = useRef<ReactPlayerType | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-  const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+  const stageRef = useRef<Konva.Stage>(null);
 
   useEffect(() => {
     const updateDimensions = () => {
@@ -32,71 +69,164 @@ const VideoReviewTool: React.FC = () => {
 
     updateDimensions();
 
-    return () => window.removeEventListener('resize', updateDimensions);
+    if (typeof window !== 'undefined') {
+      // windowオブジェクトが存在する場合のみイベントリスナーを追加
+      window.addEventListener('resize', updateDimensions);
+      return () => window.removeEventListener('resize', updateDimensions);
+    }
   }, []);
 
-  const handleAddComment = (e: any) => {
-    e.evt.preventDefault();  // イベントのデフォルト動作を防ぐ
-    e.evt.stopPropagation();  // イベントの伝播を停止
+  const [isModalVisible, setIsModalVisible] = useState(false); // モーダルの表示状態
+  const [inputComment, setInputComment] = useState(''); // モーダル内で入力されたコメント
 
-    const stage = e.target.getStage();
+  const handleAddComment = (e: any) => {
+    e.evt.preventDefault();
+    e.evt.stopPropagation();
+    const stage = stageRef.current!.getStage();
     const pointerPosition = stage.getPointerPosition();
     const newComment: Comment = {
-      x: pointerPosition.x,
-      y: pointerPosition.y,
-      text: `Comment at ${currentTime.toFixed(2)}s`,
+      x: pointerPosition!.x / dimensions.width,
+      y: pointerPosition!.y / dimensions.height,
+      text: '',
       time: currentTime,
+      videoWidth: dimensions.width,
+      videoHeight: dimensions.height,
     };
+
     setComments((prevComments) => [...prevComments, newComment]);
+    setIsModalVisible(true); // モーダルを表示
   };
 
+  const handleOk = () => {
+    if (stageRef.current) {
+      // stageRef.currentがnullでないことを確認
+      const newComments = comments.map((comment, index) => {
+        if (index === comments.length - 1) {
+          return { ...comment, text: inputComment };
+        }
+        return comment;
+      });
+      setComments(newComments);
+      setIsModalVisible(false); // モーダルを非表示
+      setInputComment(''); // 入力フィールドをリセット
+    }
+  };
+
+  const handleCancel = () => {
+    // コメント消去
+    const newComments = comments.filter(
+      (_, index) => index !== comments.length - 1,
+    );
+    setComments(newComments);
+    setIsModalVisible(false); // モーダルを非表示
+  };
+  const handleProgress = (state: {
+    playedSeconds: number;
+    played: number;
+    loadedSeconds: number;
+    loaded: number;
+  }) => {
+    setCurrentTime(state.playedSeconds);
+  };
+
+  const handleTimestampClick = (time: number) => {
+    const player = document.querySelector('video') as HTMLVideoElement;
+    player.currentTime = time;
+  };
 
   return (
-    <div
-      ref={containerRef}
-      style={{
-        position: 'relative',
-        width: '100%',
-        height: '0',
-        paddingBottom: '56.25%',
-      }}
-    >
-      <ReactPlayer
-        ref={playerRef}
-        url="assets/Big_Buck_Bunny_360_10s_1MB.mp4"
-        width="100%"
-        height="100%"
-        style={{ position: 'absolute', top: 0, left: 0 }}
-        controls={true}  // コントロールを表示
-        onError={(e) => console.error('ReactPlayer error:', e)}  // エラーをコンソールに出力
-        onReady={() => console.log('ReactPlayer is ready')}  // 準備完了時にログを出力
-        config={{ file: {
-            attributes: { crossOrigin: "anonymous" }  // CORSエラー対策
-          }}}
-      />
-      <Stage
-        width={dimensions.width }
-        height={dimensions.height - 60}
-        style={{ position: 'absolute', top: 0, left: 0 }}
-        onClick={handleAddComment}
+    <div>
+      <div
+        ref={containerRef}
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '0',
+          paddingBottom: '56.25%',
+        }}
       >
-        <Layer>
-          {comments.map((comment, index) => (
-            <React.Fragment key={index}>
-              <Circle x={comment.x} y={comment.y} radius={5} fill="red" />
-              <Text
-                x={comment.x + 10}
-                y={comment.y + 10}
-                text={comment.text}
-                fontSize={12}
-                fill="white"
-                stroke="black"
-                strokeWidth={0.5}
-              />
-            </React.Fragment>
-          ))}
-        </Layer>
-      </Stage>
+        <ReactPlayerMemo handleProgress={handleProgress} />
+        <Stage
+          ref={stageRef}
+          width={dimensions.width}
+          height={dimensions.height - 60}
+          style={{ position: 'relative', top: 0, left: 0 }}
+          onClick={handleAddComment}
+        >
+          <Layer>
+            {comments
+              .filter((comment) => Math.abs(comment.time - currentTime) <= 0.5)
+              .map((comment, index) => (
+                <React.Fragment key={index}>
+                  <Group key={index}>
+                    <Circle
+                      x={comment.x * dimensions.width}
+                      y={comment.y * dimensions.height}
+                      radius={10}
+                      fill="red"
+                    />
+                    <Line
+                      points={[
+                        comment.x * dimensions.width,
+                        comment.y * dimensions.height,
+                        comment.x * dimensions.width + 10,
+                        comment.y * dimensions.height + 10,
+                        comment.x * dimensions.width + 110,
+                        comment.y * dimensions.height + 10,
+                        comment.x * dimensions.width + 110,
+                        comment.y * dimensions.height + 60,
+                        comment.x * dimensions.width - 10,
+                        comment.y * dimensions.height + 60,
+                        comment.x * dimensions.width - 10,
+                        comment.y * dimensions.height + 10,
+                        comment.x * dimensions.width,
+                        comment.y * dimensions.height,
+                      ]}
+                      closed
+                      fill="white"
+                      stroke="black"
+                      strokeWidth={2}
+                    />
+                    <Text
+                      x={comment.x * dimensions.width + 15}
+                      y={comment.y * dimensions.height + 15}
+                      text={comment.text}
+                      fontSize={12}
+                      fill="black"
+                      width={90}
+                      padding={5}
+                      align="center"
+                    />
+                  </Group>
+                </React.Fragment>
+              ))}
+          </Layer>
+        </Stage>
+        <Modal
+          title="Add Comment"
+          open={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <Input
+            value={inputComment}
+            onChange={(e) => setInputComment(e.target.value)}
+          />
+        </Modal>
+      </div>
+      <div className="pt-8 mt-8 space-y-4">
+        {comments.map((comment, index) => (
+          <div key={index} className="flex items-center space-x-2">
+            <p className="text-gray-800">{comment.text}</p>
+            <button
+              className="text-blue-500 hover:underline"
+              onClick={() => handleTimestampClick(comment.time)}
+            >
+              {comment.time.toFixed(2)}s
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,10 +1,5 @@
 import VideoReviewTool from '@/app/video/VideoReviewTool';
 
 export default function Home() {
-  return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Video Review Tool</h1>
-      <VideoReviewTool />
-    </main>
-  );
+  return <VideoReviewTool />;
 }
